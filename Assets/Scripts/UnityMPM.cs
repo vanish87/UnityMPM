1f4b38e1e4492d697fd6b814e18b7001e3490f7a
Yuan Li <vanish8.7@gmail.com>
2020-10-23 10:48:09 +0900

    first init

---
+++ Assets/Scripts/UnityMPM.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace UnityMPM
+{
+
+    public class Particle
+    {
+        public float2 pos;
+
+    }
+    public class Grid
+    {
+
+    }
+    public class UnityMPM : MonoBehaviour
+    {
+        protected void Init()
+        {
+
+        }
+        protected void Step()
+        {
+
+        }
+
+        protected void P2G()
+        {
+
+        }
+        public void G2P()
+        {
+
+        }
+    }
+}
